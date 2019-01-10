07537d8d5d7f9498de88832ab2c5d5ae77006169
Krishnaswamy <rkrishnaswamy@Talisentech.local>
2019-01-09 19:51:14 -0600

    webpack or production

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (env) => {
   return {
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'public', 'dist'),
     filename: 'bundle.js'
   },
   module: {
@@ -37,7 +37,8 @@ module.exports = (env) => {
   devtool: isProduction ? 'source-map' : 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true //used to serve index.html when 404 eg: /create
+    historyApiFallback: true, //used to serve index.html when 404 eg: /create,
+    publicPath: '/dist/'
   }
 }
 };
