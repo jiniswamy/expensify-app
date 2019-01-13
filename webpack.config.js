2f0a57c886b3251f126dc48454fc025b89f9802e
Krishnaswamy <rkrishnaswamy@Talisentech.local>
2019-01-13 17:29:41 -0600

    removed package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
-process.env.NODE_ENV = prcess.env.NODE_ENV || 'development'; //env.test 0r env.development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'; //env.test 0r env.development
 
 if(process.env.NODE_ENV === 'test'){
   require('dotenv').config({path: '.env.test'});
